0db0fc3840762d0b2cc9cdcea70051afffba5920
Rafael <rafael.andrade.dev0@gmail.com>
2021-03-27 08:59:25 -0300

    Retira bottom do header

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -5,11 +5,11 @@ const Header = () => {
   return (
     <>
       <Navbar
-        className={`mr-auto mx-auto ${styles.navbar_heigth}`}
+        className={`mr-auto mx-auto ${styles.navbar_heigth} ${styles.bg_custom_2}`}
         collapseOnSelect
         expand="lg"
         bg="light"
-        variant="light"
+        variant="dark"
       >
         <Navbar.Brand href="#home">Totally NOT Ign ¯\_(ツ)_/¯</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
@@ -27,29 +27,6 @@ const Header = () => {
           </Nav>
         </Navbar.Collapse>
       </Navbar>
-
-      <div className={styles.header}>
-        <ul>
-          <li>
-            <a href="/content/wiki/">Guias Completos</a>
-          </li>
-          <li>
-            <a href="/games/86295/feature/os-games-mais-aguardados-de-2021">
-              Games mais esperados de 2021
-            </a>
-          </li>
-          <li>
-            <a href="/playstation-4-ps4/57806/feature/os-25-melhores-games-do-playstation-4">
-              Os 25 melhores jogos de PS4
-            </a>
-          </li>
-          <li>
-            <a href="/xbox-one/83199/feature/os-25-melhores-jogos-do-xbox-one">
-              Os 25 melhores jogos de Xbox One
-            </a>
-          </li>
-        </ul>
-      </div>
     </>
   );
 };
