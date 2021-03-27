8e8362e130978f3d33f5780c7c26f33d869d1032
Rafael <rafael.andrade.dev0@gmail.com>
2021-03-27 11:19:41 -0300

    Adicionar styles ao footer

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,5 +1,103 @@
+import styles from "./Footer.module.css";
+
 const Footer = () => {
-  return <h1>Footer</h1>;
+  return (
+    <footer className={`bg-dark text-white ${styles.footer_b} mt-5`}>
+      <div className="container">
+        <div className="row">
+          <div className="col-12 col-md">
+            <h4>Totally NOT IGN ¯\_(ツ)_/¯</h4>
+            <small className="d-block mb-3 text-muted">&copy;2021</small>
+          </div>
+          <div className="col-6 col-md">
+            <h5>Reviews</h5>
+            <ul className="list-unstyled text-small">
+              <li>
+                <a className="text-muted" href="#">
+                  Cool stuff
+                </a>
+              </li>
+              <li>
+                <a className="text-muted" href="#">
+                  Random feature
+                </a>
+              </li>
+              <li>
+                <a className="text-muted" href="#">
+                  Team feature
+                </a>
+              </li>
+              <li>
+                <a className="text-muted" href="#">
+                  Stuff for developers
+                </a>
+              </li>
+              <li>
+                <a className="text-muted" href="#">
+                  Another one
+                </a>
+              </li>
+              <li>
+                <a className="text-muted" href="#">
+                  Last time
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="col-6 col-md">
+            <h5>News</h5>
+            <ul className="list-unstyled text-small">
+              <li>
+                <a className="text-muted" href="#">
+                  Resource
+                </a>
+              </li>
+              <li>
+                <a className="text-muted" href="#">
+                  Resource name
+                </a>
+              </li>
+              <li>
+                <a className="text-muted" href="#">
+                  Another resource
+                </a>
+              </li>
+              <li>
+                <a className="text-muted" href="#">
+                  Final resource
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="col-6 col-md">
+            <h5>Talk to us</h5>
+            <ul className="list-unstyled text-small">
+              <li>
+                <a className="text-muted" href="#">
+                  Team
+                </a>
+              </li>
+              <li>
+                <a className="text-muted" href="#">
+                  Locations
+                </a>
+              </li>
+              <li>
+                <a className="text-muted" href="#">
+                  Privacy
+                </a>
+              </li>
+              <li>
+                <a className="text-muted" href="#">
+                  Terms
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
